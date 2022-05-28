4e75f5c3cac56bde690b5e450ee8570240e0842b
Mayank Chaturvedi <chaturvedim4@gmail.com>
2022-05-28 20:42:16 +0530

    working without delete functionality

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,41 @@
-import React, { useEffect,useState } from 'react'
-import {collection, getDocs} from "firebase/firestore";
-import {db} from "../firebase-config";
-const Home = () => {
+import React, { useCallback, useEffect, useState } from "react";
+import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
+import { db,auth } from "../firebase-config";
+const Home = ({isAuth}) => {
   const [articleList, setArticleList] = useState([]);
-  const articleCollectionRef = collection(db,"articles");
-  useEffect(
-    ()=>{
-      const getArticles = async ()=>{//creating a new function because we need to do this asynchronously
-        const data = await getDocs(articleCollectionRef)
-        setArticleList(data.docs.map((doc)=>({...doc.data(),id:doc.id})));
-      };
-      console.log("here")
-      getArticles()
-    }
-  );
+  useEffect(() => {
+    const articleCollectionRef = collection(db, "articles");
+    const getArticles = async () => {
+      //creating a new function because we need to do this asynchronously
+      const data = await getDocs(articleCollectionRef);
+      setArticleList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    console.log("here");
+    
+    getArticles();
+  }, []);
+  //empty dependency => [] means call function only on first mount(may be on end also until app not in production)
+
+ 
+  
+
   return (
-    <div className='homePage'>
-      {
-        articleList.map(
-          (article)=>{
-            return <div className="post">{article.title}</div>
-          }
-        )
-      }
+    <div className="homePage">
+      {articleList.map((article) => {
+        return (
+          <div className="post">
+            <div className="postHeader">
+              <div className="title">
+                <h1>{article.title}</h1>
+              </div>
+            </div>
+            <div className="postTextContainer">{article.postText}</div>
+            <h3>{article.author.name}</h3>
+          </div>
+        );
+      })}
     </div>
-  )
-}
+  );
+};
 
 export default Home;
